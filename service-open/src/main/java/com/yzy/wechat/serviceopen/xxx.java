8ebdfadb0051f1a2437f423685abc8513257c98c
颜德洪 <1369347635@qq.com>
2018-02-27 17:23:55 +0800

    test

--- service-open/src/main/java/com/yzy/wechat/serviceopen/xxx.java
+++ service-open/src/main/java/com/yzy/wechat/serviceopen/xxx.java
@@ -5,6 +5,6 @@ package com.yzy.wechat.serviceopen;
  */
 public class xxx {
     public static void main(String[] args) {
-        System.out.println("hhh");
+        System.out.println("hh41654h");
     }
 }
